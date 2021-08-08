14fb4ca023140a23f6ab6f3343ae69f91b4e76c3
Prime <academy@Primes-MacBook-Pro.local>
2021-08-07 19:40:28 -0500

    Completed assignment part-3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,111 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+//Create a global variable named `basket` and set it to an empty array.
+
+let basket = [];
+
+
+
+//Create a function called `addItem`. It should:
+  //take an input parameter for a string `item`
+  //add the new item to the global array `basket`.
+  //return `true` indicating the item was added
+
+  function addItem(item){
+    basket.push(item);
+    if (basket.includes(item)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  console.log(addItem('apple'));
+  console.log(addItem('mango'));
+
+  // For example, to test `addItem`:
+
+  console.log(`Basket is ${basket}`);
+  console.log('Adding apples (expect true)', addItem('apples'));
+  console.log(`Basket is now ${basket}`);
+
+
+  //Create a function called `listItems`. It should:
+    // loop over the items in the `basket` array
+    // console.log each individual item on a new line
+
+    function listItems(){
+      for (let item = 0; item < basket.length; item++){
+        console.log(basket[item])
+      }
+    }
+
+    listItems();
+
+
+    // Create a function called `empty`. It should:
+    //   reset the `basket` to an empty array
+
+    function empty(){
+      basket = [];
+      return basket;
+    }
+
+    empty()
+
+    console.log(basket)
+
+    //Add a global `const` named `maxItems` and set it to 5.
+
+    const maxItems = 5;
+
+    // Create a function called isFull(). It should:
+    //   return `false` if the basket contains *less* than max number of items
+    //   return `true` otherwise (equal or more than maxItems)
+
+    function isFull(){
+      if (basket.length >= maxItems){
+        return true;
+      }
+      return false;
+    }
+
+    console.log(isFull());
+
+    // Update the required `addItem` function to:
+    //   Use the `isFull` function to prevent more than `maxItems` from being added to the basket.
+    //   If an item was added to the array, return `true`
+    //   If there was no room and the item could not be added return `false`
+
+
+addItem('kiwi');
+addItem('watermelon');
+addItem('dates');
+console.log(basket)
+function newAddItem(){
+if (isFull()){
+  console.log(`We don't have an availability in the room`)
+  return true;
+}
+console.log(`We have an availability in the room`)
+return false;
+}
+
+console.log(newAddItem())
+    // Create a function called `removeItem`. It should:
+    //   Take an input parameter for a string `item`
+    //   Use [Array.indexOf] to find the index of the first matching item in the basket.
+    //   Use [Array.splice] to remove the first matching item from the basket.
+    //   Return the item removed or `null` if the item was not found
+
+
+function removeItem(item){
+  let index = basket.indexOf(item);
+  basket.splice(index, 2)
+  return basket;
+}
+
+console.log(removeItem('kiwi'))
--- readme.md
+++ readme.md
@@ -10,9 +10,9 @@ This week we'll be pulling together all of the Javascript techniques you've lear
 - functions
 - HTML & CSS
 
-## Assignment - Cart System 
+## Assignment - Cart System
 
-In this section we will write some functions that might be used in a simple cart system for a store. 
+In this section we will write some functions that might be used in a simple cart system for a store.
 
 ### Required Features
 Update the `cart.js` file to do the following:
@@ -21,7 +21,7 @@ Update the `cart.js` file to do the following:
 
 - Create a function called `addItem`. It should:
   - take an input parameter for a string `item`
-  - add the new item to the global array `basket`. 
+  - add the new item to the global array `basket`.
   - return `true` indicating the item was added
 
 - Create a function called `listItems`. It should:
@@ -41,7 +41,7 @@ console.log('Adding apples (expect true)', addItem('apples'));
 console.log(`Basket is now ${basket}`);
 ```
 
-### Stretch Goals 
+### Stretch Goals
 Remember that Stretch Goals are not required, but will help you to further develop concepts from the skills we have covered.
 
 __Using functions in other functions!__
@@ -53,17 +53,17 @@ __Using functions in other functions!__
   - return `true` otherwise (equal or more than maxItems)
 
 3. Update the required `addItem` function to:
-  - Use the `isFull` function to prevent more than `maxItems` from being added to the basket. 
+  - Use the `isFull` function to prevent more than `maxItems` from being added to the basket.
   - If an item was added to the array, return `true`
   - If there was no room and the item could not be added return `false`
 
 __Using Array built-in functions!__
 
-4. Create a function called `removeItem`. It should:
-  - Take an input parameter for a string `item`
-  - Use [Array.indexOf](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf) to find the index of the first matching item in the basket.
-  - Use [Array.splice](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice) to remove the first matching item from the basket.
-  - Return the item removed or `null` if the item was not found
+Create a function called `removeItem`. It should:
+  Take an input parameter for a string `item`
+  Use [Array.indexOf] to find the index of the first matching item in the basket.
+  Use [Array.splice] to remove the first matching item from the basket.
+  Return the item removed or `null` if the item was not found
 
 ## Assignment Submission
 Check in your repo, then turn in your work via the Prime Academy Assignment Application at http://primeacademy.io, as usual and don't hesitate to hit up the Slack channel as needed!
